03a8be5e302dbd5f6f9428add9965b60c3d2cc28
Yousef <yousefsameh24@gmail.com>
2023-09-05 22:50:21 +0300

    create rest operations

--- app.js
+++ app.js
@@ -13,9 +13,21 @@ const employees = [
 
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-  res.json(req.body);
-});
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+  
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+  
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+  
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  }); 
 
 app.get('/employees', (req, res) => {
     const { firstName, lastName, age } = req.query;
