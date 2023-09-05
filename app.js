2ef604f274ad2bd61a12a3bab240b9aaecb8b409
Yousef <yousefsameh24@gmail.com>
2023-09-05 22:55:43 +0300

    create user resource

--- app.js
+++ app.js
@@ -29,6 +29,22 @@ app.get('/', (req, res) => {
     return res.send('Received a DELETE HTTP method');
   }); 
 
+  app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+  });
+  
+  app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+  });
+  
+  app.put('/users/:userId', (req, res) => {
+    return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+  });
+  
+  app.delete('/users/:userId', (req, res) => {
+    return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+  });
+
 app.get('/employees', (req, res) => {
     const { firstName, lastName, age } = req.query;
     let results = [...employees];
