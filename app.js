0032442bc4bee858628cfdfb6c573cac8d0a3f37
Yousef <yousefsameh24@gmail.com>
2023-09-05 20:02:58 +0300

    handle queries

--- app.js
+++ app.js
@@ -3,10 +3,35 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// employees data in a database
+const employees = [
+    { firstName: 'Jane', lastName: 'Smith', age: 20 },
+    //...
+    { firstName: 'John', lastName: 'Smith', age: 30 },
+    { firstName: 'Mary', lastName: 'Green', age: 50 },
+]
+
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
   res.json(req.body);
 });
+
+app.get('/employees', (req, res) => {
+    const { firstName, lastName, age } = req.query;
+    let results = [...employees];
+    if (firstName) {
+      results = results.filter(r => r.firstName === firstName);
+    }
+  
+    if (lastName) {
+      results = results.filter(r => r.lastName === lastName);
+    }
+  
+    if (age) {
+      results = results.filter(r => +r.age === +age);
+    }
+    res.json(results);
+  });
 
 app.listen(3000, () => console.log('server started'));
