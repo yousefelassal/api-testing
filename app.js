6b2dfc6b103b71b121fb0f333f07e3814e086e4a
Yousef <yousefsameh24@gmail.com>
2023-09-06 00:33:01 +0300

    use routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
 const models = require('./models');
+const routes = require('./routes');
 
 const app = express();
 
@@ -16,6 +16,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
 // employees data in a database
 const employees = [
     { firstName: 'Jane', lastName: 'Smith', age: 20 },
@@ -25,50 +29,6 @@ const employees = [
 ]
 
 app.use(bodyParser.json());
-
-app.get('/session', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-  });
-  
-  app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users));
-  });
-  
-  app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId]);
-  });
-  
-  app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
-  });
-  
-  app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-  });
-  
-  app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-      id,
-      text: req.body.text,
-      userId: req.context.me.id,
-    };
-  
-    req.context.models.messages[id] = message;
-  
-    return res.send(message);
-  });
-  
-  app.delete('/messages/:messageId', (req, res) => {
-    const {
-      [req.params.messageId]: message,
-      ...otherMessages
-    } = req.context.models.messages;
-  
-    req.context.models.messages = otherMessages;
-  
-    return res.send(message);
-  });
 
 app.get('/employees', (req, res) => {
     const { firstName, lastName, age } = req.query;
