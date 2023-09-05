6698fe8549af36a9a09c6402ffa1f61d6246d01a
Yousef <yousefsameh24@gmail.com>
2023-09-06 00:21:22 +0300

    update app

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const models = require('./models');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
+});
 
 // employees data in a database
 const employees = [
@@ -15,95 +24,48 @@ const employees = [
     { firstName: 'Mary', lastName: 'Green', age: 50 },
 ]
 
-let users = {
-    1: {
-      id: '1',
-      username: 'Robin Wieruch',
-    },
-    2: {
-      id: '2',
-      username: 'Dave Davids',
-    },
-  };
-  
-  let messages = {
-    1: {
-      id: '1',
-      text: 'Hello World',
-      userId: '1',
-    },
-    2: {
-      id: '2',
-      text: 'By World',
-      userId: '2',
-    },
-  };
-
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
+app.get('/session', (req, res) => {
+    return res.send(req.context.models.users[req.context.me.id]);
   });
   
-  app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-  });
-  
-  app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-  });
-  
-  app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
-  }); 
-
   app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
+    return res.send(Object.values(req.context.models.users));
   });
   
   app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
+    return res.send(req.context.models.users[req.params.userId]);
   });
   
-  app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
-  });
-  
-  app.put('/users/:userId', (req, res) => {
-    return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-  });
-  
-  app.delete('/users/:userId', (req, res) => {
-    return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-  });
-
   app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
+    return res.send(Object.values(req.context.models.messages));
   });
   
   app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
+    return res.send(req.context.models.messages[req.params.messageId]);
   });
-
+  
   app.post('/messages', (req, res) => {
     const id = uuidv4();
     const message = {
       id,
       text: req.body.text,
+      userId: req.context.me.id,
     };
   
-    messages[id] = message;
+    req.context.models.messages[id] = message;
   
     return res.send(message);
   });
-
+  
   app.delete('/messages/:messageId', (req, res) => {
     const {
       [req.params.messageId]: message,
       ...otherMessages
-    } = messages;
+    } = req.context.models.messages;
   
-    messages = otherMessages;
+    req.context.models.messages = otherMessages;
   
     return res.send(message);
   });
